434d3cb69008e20f8da81086b0cca135730c47f7
iirrrrffan <irfanali376246@gmail.com>
2024-02-22 18:07:59 +0530

    all over

--- BackendProject/routers/userRoute.js
+++ BackendProject/routers/userRoute.js
@@ -4,14 +4,14 @@ const userController=require("../contoller/userController")
  const trycatch=require("../middleware/tryCatchMiddleware")
  const varifyTocken = require("../middleware/userAuthMiddleware")
 
-
+ 
 //   .use(varifyTocken)
  userRouter.post("/register",trycatch(userController.createUser))
  userRouter.post("/login",trycatch(userController.userLogin))
  userRouter.get("/products",trycatch(userController.productList))
  userRouter.get("/products/:id",trycatch(userController.productGetById))
 
- 
+
  userRouter.get("/products/category/:categoryname",varifyTocken,trycatch(userController.ProductByCategory))
  userRouter.post("/:id/cart",trycatch(userController.addToCart)) 
 userRouter.delete("/:id/cart",trycatch(userController.deleteFromCart))
@@ -23,9 +23,8 @@ userRouter.post("/:id/addtowishList",varifyTocken,trycatch(userController.addWis
 userRouter.get("/:id/wishList",varifyTocken,trycatch(userController.showWishList))
 userRouter.delete("/:id/wishList",varifyTocken,trycatch(userController.deleteWishList))
 userRouter.post("/:id/payment",varifyTocken,trycatch(userController.payment))
-userRouter.get('/payment/success',varifyTocken,trycatch(userController.success))
+userRouter.get('/payment/success',trycatch(userController.success))
 userRouter.post('/payment/cancel',varifyTocken,trycatch(userController.cancel))
 userRouter.get('/order',varifyTocken,trycatch(userController.showOrders))
-
 
  module.exports=userRouter; 
